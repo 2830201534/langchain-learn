fbc6581f1cc54b49d9923fb3dc27b5c49ef1b7c8
yzpd <yzpd@yzpddeMac-mini.local>
2026-03-23 16:25:07 +0800

    Initial commit: LangChain 7-day learning plan

---
+++ activate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 激活 LangChain 学习环境
+source /opt/homebrew/Caskroom/miniconda/base/bin/activate
+conda activate langchain
+echo "LangChain 环境已激活"
+
+# 进入学习目录
+cd /Users/yzpd/work/pycharm-project/langchain-learn
+echo "当前目录: $(pwd)"
