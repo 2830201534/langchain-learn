2a4149d57cdb68466e0060c7c083404f366fc9aa
yzpd <2830201534@qq.com>
2026-03-23 20:44:56 +0800

    update

--- activate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 激活 LangChain 学习环境
-source /opt/homebrew/Caskroom/miniconda/base/bin/activate
-conda activate langchain
-echo "LangChain 环境已激活"
-
-# 进入学习目录
-cd /Users/yzpd/work/pycharm-project/langchain-learn
-echo "当前目录: $(pwd)"
